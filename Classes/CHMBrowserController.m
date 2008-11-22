f13becab4e8226b875866335260edfbe03ec3150
Robin Lu <iamawalrus@gmail.com>
2008-11-22 21:13:28 +0800

    rotate as safari

--- Classes/CHMBrowserController.m
+++ Classes/CHMBrowserController.m
@@ -127,7 +127,7 @@
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
     // Return YES for supported orientations
-    return YES;
+    return interfaceOrientation != UIDeviceOrientationPortraitUpsideDown;
 }
 
 - (void)didReceiveMemoryWarning {
